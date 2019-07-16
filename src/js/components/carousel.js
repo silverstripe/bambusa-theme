cd164e8afdcf02f7afa1d93a53e158ebe26f2bec
Sacha Judd <sjudd@silverstripe.com>
2019-07-17 11:37:22 +1200

    FIX Add accessibility requirements for carousel, remove btn-link styling

--- src/js/components/carousel.js
+++ src/js/components/carousel.js
@@ -10,6 +10,7 @@ export default function () {
     const $carousel = $(this);
     const $carouselPause = $carousel.find('.carousel-pause');
     const $carouselPlay = $carousel.find('.carousel-play');
+    const $carouselSlideTitle = $carousel.find('.carousel-slide-title');
 
     $carousel.carousel({
       wrap: true,
@@ -26,6 +27,17 @@ export default function () {
       $carousel.carousel('cycle');
       $carouselPlay.hide();
       $carouselPause.show().focus();
+    });
+
+    const $carouselControlButtons = $carousel.find('.carousel-controls button, .carousel-indicators button');
+
+    $carouselControlButtons.click(() => {
+      // Set a timeout for the duration of the CSS animation that switches the slide.
+      // The new slide does not become "active" until the animation is complete.
+      setTimeout(() => {
+        const $activeSlide = $carousel.find('.carousel-item.active');
+        $carouselSlideTitle.text($activeSlide.data('title'));
+      }, 700);
     });
   });
 }
